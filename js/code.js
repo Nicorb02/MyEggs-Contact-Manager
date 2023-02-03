6bb55a186e2952c3cb09cdbb1045df7c29aa3994
jenna3708 <53448385+jenna3708@users.noreply.github.com>
2023-02-03 14:49:57 -0500

    new egg:(

--- js/code.js
+++ js/code.js
@@ -88,7 +88,7 @@ function doRegister(){
 
 
 
-    var tmp = {FirstName:firstName, LastName:lastName, Username:newUser, Pwd:password};
+    var tmp = {FirstName:firstName, LastName:lastName, Username:newEgg, Pwd:password};
     var jsonPayload = JSON.stringify( tmp );
 
     var url = urlBase + '/Register.' + extension;
